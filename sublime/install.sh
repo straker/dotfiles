3616f5bfe816765762232ead98b2c8eefbe6f1be
straker <stevenlambert503@gmail.com>
2018-03-02 00:47:59 -0700

    install default sublime text packages

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo ''
-echo -e "\033[1mCopying Sublime Text settings\033[0m"
-echo '#############################'
+echo -e "\033[1mCopying Sublime Text settings and installing packages\033[0m"
+echo '#####################################################'
 echo ''
 
 # Open subl to ensure the Application Support directory exists
@@ -14,4 +14,8 @@ subl
 for fileglob in ~/Library/Application\ Support/Sublime\ Text*/; do
   cp "$DIR/sublime/Preferences.sublime-settings" "$fileglob/Packages/User/Preferences.sublime-settings"
   cp "$DIR/sublime/Default (OSX).sublime-keymap" "$fileglob/Packages/User/Default (OSX).sublime-keymap"
+
+  # Install Sublime Text Package Control and default packages
+  curl -o "$fileglob/Installed Packages/Package Control.sublime-package" https://packagecontrol.io/Package%20Control.sublime-package
+  cp "$DIR/sublime/Package Control.sublime-settings" "$fileglob/Packages/User/Package Control.sublime-settings"
 done
