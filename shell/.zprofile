59552fb30e2243c28ea36ecdf65b0b05aed6ac37
straker <stevenlambert503@gmail.com>
2019-05-26 15:12:30 -0600

    feat: add zprofile file

---
+++ shell/.zprofile
@@ -0,0 +1,4 @@
+# load NVM for things like linting and prettier in sublime
+# @see https://github.com/SublimeLinter/
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
--- shell/install.sh
+++ shell/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo ''
-echo -e "\033[1mCopying .zshrc and shell scripts\033[0m"
+echo -e "\033[1mCopying .zshrc, .zprofile, and shell scripts\033[0m"
 echo '################################'
 echo ''
 
 cp "$DIR/shell/.zshrc" ~/
+cp "$DIR/shell/.zprofile" ~/
 cp "$DIR/shell/.shell_config" ~/
