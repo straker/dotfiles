ebdd12c2ad08f329fb144153e2b17bd7418ab5ab
straker <stevenlambert503@gmail.com>
2019-05-26 16:23:47 -0600

    feat: set default iterm preference to restore directory

--- shell/install.sh
+++ shell/install.sh
@@ -8,3 +8,7 @@ echo ''
 cp "$DIR/shell/.zshrc" ~/
 cp "$DIR/shell/.zprofile" ~/
 cp "$DIR/shell/.shell_config" ~/
+
+# set default iterm preference to reuse previous session's directory
+# requires restart
+/usr/libexec/PlistBuddy -c "Set :'New Bookmarks':0:'Custom Directory' Recycle" ~/Library/Preferences/com.googlecode.iterm2.plist
