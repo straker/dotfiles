ac1755f5bf1aba5a8253ac60cdac4b25769d6136
straker <2433219+straker@users.noreply.github.com>
2022-11-20 12:13:00 -0700

    set DIR if not set

--- git/install.sh
+++ git/install.sh
@@ -5,6 +5,10 @@ echo -e "\033[1mCopying .gitattributes, .gitconfig, and .gitignore\033[0m"
 echo '######################################'
 echo ''
 
+if ! declare -p DIR &> /dev/null; then
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+fi
+
 cp "$DIR/git/.gitattributes" ~/
 cp "$DIR/git/.gitconfig" ~/
 cp "$DIR/git/.gitignore" ~/
--- shell/install.sh
+++ shell/install.sh
@@ -5,6 +5,10 @@ echo -e "\033[1mCopying .zshrc, .zprofile, and shell scripts\033[0m"
 echo '################################'
 echo ''
 
+if ! declare -p DIR &> /dev/null; then
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+fi
+
 cp "$DIR/shell/.zshrc" ~/
 cp "$DIR/shell/.zprofile" ~/
 cp "$DIR/shell/.shell_config" ~/
